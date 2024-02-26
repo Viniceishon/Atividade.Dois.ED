339baf8598185a2448544b7b793ffcc6fb2fce11
Vinicius Garcia Silva <129995933+Viniceishon@users.noreply.github.com>
2024-02-26 00:06:07 -0300

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,6 @@
+public class Main2 {
+    public static void main(String[] args) {
+        Cachorro cachorro = new Cachorro(1, "Doméstico", "Terrestre", "Rex", "Labrador", 5);
+        System.out.println(cachorro.toString());
+    }
+}
